39eb92872f5d222c86d839ba53d00290162dd511
Michał Stanik <micha.stanik@gmail.com>
2020-01-01 19:47:51 +0100

    Initial Flogging project

---
+++ MST/MST.Logging.Core/FlogDetail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MST.Flogging.Core
+{
+    public class FlogDetail
+    {
+        public FlogDetail()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public DateTime Timestamp { get; private set; }
+        public string Message { get; set; }
+
+        //WHERE
+        public string Product { get; set; }
+        public string Layer { get; set; }
+        public string Location { get; set; }
+        public string  Hostname { get; set; }
+
+        //WHO
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+
+        //Everything Else
+        public long? ElapsedMilliseconds { get; set; }
+        public Exception Exception { get; set; }
+        public Dictionary<string, object> AdditionalInfo { get; set; }
+    }
+}
---
+++ MST/MST.Logging.Core/Flogger.cs
@@ -0,0 +1,34 @@
+﻿using Serilog;
+using System;
+using System.IO;
+
+namespace MST.Flogging.Core
+{
+    public static class Flogger
+    {
+        private static readonly ILogger _perfLogger;
+        private static readonly ILogger _usageLogger;
+        private static readonly ILogger _errorLogger;
+        private static readonly ILogger _diagnosticLogger;
+
+        static Flogger()
+        {
+            _perfLogger = new LoggerConfiguration()
+                .WriteTo.File(path: Path.Combine(AppContext.BaseDirectory, "Logs\\perf.txt"))
+                .CreateLogger();
+
+            _usageLogger = new LoggerConfiguration()
+                .WriteTo.File(path: Path.Combine(AppContext.BaseDirectory, "Logs\\usage.txt"))
+                .CreateLogger();
+
+            _errorLogger = new LoggerConfiguration()
+                .WriteTo.File(path: Path.Combine(AppContext.BaseDirectory, "Logs\\error.txt"))
+                .CreateLogger();
+
+            _diagnosticLogger = new LoggerConfiguration()
+                .WriteTo.File(path: Path.Combine(AppContext.BaseDirectory, "Logs\\diagnostic.txt"))
+                .CreateLogger();
+        }
+
+    }
+}
